6c8de78b6e01be2113609b6537286524ed823f9a
Arthur Novello <novelloarthur@gmail.com>
2019-09-18 15:56:14 -0300

    velocildade rpm temperatura

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
 import obd
+from time import sleep
+
+connection = obd.OBD(check_voltage=False)
+connection.status()
 
 
-def connect():
-    connection = obd.OBD()
-    return connection.status()
+def vel():
+    return connection.query(obd.commands.SPEED)
 
 
-connect()
+def rpm():
+    return connection.query(obd.commands.RPM)
+
+
+def temp():
+    return connection.query(obd.commands.COOLANT_TEMP)
+
+
+velocidade = vel()
+rotacao = rpm()
+temperatura = temp()
+
+while True:
+    sleep(1)
+    if velocidade != vel():
+        velocidade = vel()
+
+    if rotacao != rpm():
+        rotacao = rpm()
+
+    if temperatura != temp():
+        temperatura = temp()
+
+    print(velocidade)
+    print(rotacao)
+    print(temperatura)
